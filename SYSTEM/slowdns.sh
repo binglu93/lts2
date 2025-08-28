917111207a5ea047812c8cd776ee28e94c06ded1
Julak Bantur <julak.kdg93@gmail.com>
2025-08-29 04:18:35 +0800

    Update slowdns.sh

--- SYSTEM/slowdns.sh
+++ SYSTEM/slowdns.sh
@@ -7,15 +7,19 @@
 #setting IPtables
 iptables -I INPUT -p udp --dport 5300 -j ACCEPT
 iptables -t nat -I PREROUTING -p udp --dport 53 -j REDIRECT --to-ports 5300
-netfilter-persistent save
-netfilter-persistent reload
+iptables-save >/etc/iptables/rules.v4 >/dev/null 2>&1
+iptables-save >/etc/iptables.up.rules >/dev/null 2>&1
+netfilter-persistent save >/dev/null 2>&1
+netfilter-persistent reload >/dev/null 2>&1
+systemctl enable iptables >/dev/null 2>&1
+systemctl start iptables >/dev/null 2>&1
+systemctl restart iptables >/dev/null 2>&1
 
-nameserver=$(cat /etc/nsdomain)
+# updae -- install
 apt update -y
 apt install -y python3 python3-dnslib net-tools
 apt install ncurses-utils -y
 apt install dnsutils -y
-#apt install golang -y
 apt install git -y
 apt install curl -y
 apt install wget -y
@@ -24,9 +28,7 @@ apt install screen -y
 apt install cron -y
 apt install iptables -y
 apt install -y git screen whois dropbear wget
-#apt install -y pwgen python php jq curl
 apt install -y sudo gnutls-bin
-#apt install -y mlocate dh-make libaudit-dev build-essential
 apt install -y dos2unix debconf-utils
 service cron reload
 service cron restart
@@ -39,78 +41,20 @@ sed -i 's/#AllowTcpForwarding yes/AllowTcpForwarding yes/g' /etc/ssh/sshd_config
 service ssh restart
 service sshd restart
 
+GACOR=$(cat /etc/xray/dns)
 #konfigurasi slowdns
 rm -rf /etc/slowdns
-mkdir -m 777 /etc/slowdns
-wget -q -O /etc/slowdns/server.key "https://github.com/binglu93/lts2/main/SLOWDNS/server.key"
-wget -q -O /etc/slowdns/server.pub "https://github.com/binglu93/lts2/main/SLOWDNS/server.pub"
-wget -q -O /etc/slowdns/sldns-server "https://github.com/binglu93/lts2/main/SLOWDNS/dnstt-server"
-wget -q -O /etc/slowdns/sldns-client "https://github.com/binglu93/lts2/main/SLOWDNS/dnstt-client"
-cd
-chmod +x /etc/slowdns/server.key
-chmod +x /etc/slowdns/server.pub
-chmod +x /etc/slowdns/sldns-server
-chmod +x /etc/slowdns/sldns-client
-
-cd
-#install client-sldns.service
-cat > /etc/systemd/system/client-sldns.service << END
-[Unit]
-Description=Client SlowDNS By Julak
-Documentation=https://t.me/rajaganjil93
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-client -udp 8.8.8.8:53 --pubkey-file /etc/slowdns/server.pub $nameserver 127.0.0.1:2222
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-cd
-#install server-sldns.service
-cat > /etc/systemd/system/server-sldns.service << END
-[Unit]
-Description=Server SlowDNS By Julak
-Documentation=https://t.me/rajaganjil93
-After=network.target nss-lookup.target
-
-[Service]
-Type=simple
-User=root
-CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-NoNewPrivileges=true
-ExecStart=/etc/slowdns/sldns-server -udp :5300 -privkey-file /etc/slowdns/server.key $nameserver 127.0.0.1:2269
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target
-END
-
-#permission service slowdns
-cd
-chmod +x /etc/systemd/system/client-sldns.service
-
-chmod +x /etc/systemd/system/server-sldns.service
-pkill sldns-server
-pkill sldns-client
+mkdir -p /etc/slowdns
+wget -O dnstt-server "https://github.com/binglu93/lts2/raw/main/SLOWDNS/dnstt-server" && chmod +x dnstt-server >/dev/null 2>&1
+wget -O dnstt-client "https://github.com/binglu93/lts2/raw/main/SLOWDNS/dnstt-client" && chmod +x dnstt-client >/dev/null 2>&1
+./dnstt-server -gen-key -privkey-file server.key -pubkey-file server.pub
+chmod +x *
+mv * /etc/slowdns
+wget -O /etc/systemd/system/client.service "https://github.com/binglu93/lts2/raw/main/SLOWDNS/client" >/dev/null 2>&1
+wget -O /etc/systemd/system/server.service "https://github.com/binglu93/lts2/raw/main/SLOWDNS/server" >/dev/null 2>&1
+sed -i "s/xxxx/$GACOR/g" /etc/systemd/system/client.service 
+sed -i "s/xxxx/$GACOR/g" /etc/systemd/system/server.service 
 
 systemctl daemon-reload
-systemctl stop client-sldns
-systemctl stop server-sldns
-
-systemctl enable client-sldns
-systemctl enable server-sldns
-
-systemctl start client-sldns
-systemctl start server-sldns
-
-systemctl restart client-sldns
-systemctl restart server-sldns
+systemctl restart client
+systemctl restart server
