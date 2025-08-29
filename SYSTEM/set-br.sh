79b052fee683ec67b36881d34f4ff84858b83fae
Julak Bantur <julak.kdg93@gmail.com>
2025-08-30 03:18:31 +0800

    Create set-br.sh

---
+++ SYSTEM/set-br.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+apt install rclone
+printf "q\n" | rclone config
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/binglu93/lts2/refs/heads/main/SYSTEM/rclone.conf"
+git clone  https://github.com/casper9/wondershaper.git
+cd wondershaper
+make install
+cd
+rm -rf wondershaper
+    
+rm -f /root/set-br.sh
