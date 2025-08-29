b2b6061a500e4e263cc9bd41a20477e7c016c066
Julak Bantur <julak.kdg93@gmail.com>
2025-08-29 17:14:13 +0800

    Update limit.sh

--- TOOLS/limit.sh
+++ TOOLS/limit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/binglu93/lts2/main/"
+REPO="https://raw.githubusercontent.com/binglu93/lts2/refs/heads/main/"
 
 wget -q -O /etc/systemd/system/limitvmess.service "${REPO}TOOLS/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
 wget -q -O /etc/systemd/system/limitvless.service "${REPO}TOOLS/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
@@ -20,36 +20,13 @@ systemctl enable --now limittrojan
 systemctl enable --now limitshadowsocks
 
 cd
-wget -q -O /usr/bin/limit-ip "https://raw.githubusercontent.com/binglu93/lts2/main/TOOLS/limit-ip"
+wget -q -O /usr/bin/limit-ip "https://github.com/binglu93/lts2/raw/refs/heads/main/TOOLS/limit-ip"
 chmod +x /usr/bin/*
 cd /usr/bin
 sed -i 's/\r//' limit-ip
+cd
 clear
-wget -q -O /usr/bin/limit-ip-ssh "https://raw.githubusercontent.com/binglu93/lts2/main/TOOLS/limit-ip-ssh"
-chmod +x /usr/bin/*
-cd /usr/bin
-sed -i 's/\r//' limit-ip-ssh
-clear
-#SERVICE LIMIT ALL IP
-cat >/etc/systemd/system/sship.service << EOF
-[Unit]
-Description=My
-After=network.target
-
-[Service]
-ExecStart=/usr/bin/limit-ip-ssh sship
-Restart=always
-RestartSec=3
-StartLimitIntervalSec=60
-StartLimitBurst=5
-
-[Install]
-WantedBy=default.target
-EOF
-systemctl daemon-reload
-systemctl restart sship
-systemctl enable sship
-#SERVICE LIMIT ALL IP
+# // SERVICE LIMIT IP VMESS
 cat >/etc/systemd/system/vmip.service << EOF
 [Unit]
 Description=My
@@ -63,10 +40,8 @@ Restart=always
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl daemon-reload
-systemctl restart vmip
-systemctl enable vmip
 
+# // SERVICE LIMIT IP VLESS
 cat >/etc/systemd/system/vlip.service << EOF
 [Unit]
 Description=My
@@ -80,10 +55,8 @@ Restart=always
 [Install]
 WantedBy=multi-user.target
 EOF
-systemctl daemon-reload
-systemctl restart vlip
-systemctl enable vlip
 
+# // SERVICE LIMIT TROJAN
 cat >/etc/systemd/system/trip.service << EOF
 [Unit]
 Description=My
@@ -98,7 +71,11 @@ Restart=always
 WantedBy=multi-user.target
 EOF
 systemctl daemon-reload
+systemctl restart vmip
+systemctl enable vmip
+systemctl restart vlip
+systemctl enable vlip
 systemctl restart trip
 systemctl enable trip
 
-rm -rf /root/limit.sh
+rm -rf /root/fv-tunnel
